023aa947a26b3ce3e55fe54793b52fa0b0f3ee07
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2021-03-06 19:32:23 +0100

    Add publish message endpoint

--- src/controllers.js
+++ src/controllers.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const Topic = require('./models/Topic');
 
 /**
@@ -22,4 +23,36 @@ exports.createSubscription = async (req, res) => {
   }
 
   res.status(200).json({ url: req.body.url, topic: topic.name });
+};
+
+/**
+ * Publish a message to topic and send HTTP requests to topic subscribers.
+ *
+ * @param {Object} req The request object.
+ * @param {Object} res The response object.
+ *
+ * @returns HTTP response based on successful or unsuccessful publish.
+ */
+exports.publishMessage = async (req, res) => {
+  const topic = await Topic.findOne({ name: req.params.topic });
+  if (!topic) res.status(404).json({ message: 'Unable to publish message.' });
+
+  const payload = {
+    topic: topic.name,
+    data: { ...req.body },
+  };
+
+  const axiosConfig = {
+    headers: { 'Content-Type': 'application/json' },
+  };
+
+  topic.subscribers.forEach(async (subscriber) => {
+    try {
+      await axios.post(subscriber, payload, axiosConfig);
+    } catch (error) {
+      console.log(subscriber);
+    }
+  });
+
+  res.status(200).json({ message: 'Message published successfuly.' });
 };
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 
-const { createSubscription } = require('./controllers');
+const { createSubscription, publishMessage } = require('./controllers');
 
 const router = express.Router();
+
+router.post('/publish/:topic', publishMessage);
 
 router.post('/subscribe/:topic', createSubscription);
 
